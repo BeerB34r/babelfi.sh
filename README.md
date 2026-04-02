14a4655277299831ce0e3f0dc8e477b9ddd8bfec
Mats de Beer <mde-beer@student.codam.nl>
2026-04-02 20:01:38 +0200

    feature: babel

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # babelfi.sh
-Telecommunication across the universe
+_Telecommunication across the universe_
+
+This is a simple bash thingy intended for use in 42 clusters as a form of
+telecommunication. if sourced in your `~/.bashrc`, `~/.zshrc` it will create and
+maintain a bash mail server thingy mabobber
+
+# interface
+## porcelain
+`babel <recipient> <subject>`
+babel writes the contents of stdin into a file `$MAILBOX/<recipient>/<subject>`
+`announcePresence [username]`
+announces to the special mailbox `present` that you (or provided username) are
+present at your computer
+
+the environment variable `FRIENDS` acts as an interest list for the special
+mailbox `present`. whenever a user matching a name in your `FRIENDS` variable
+announces their presence you will get a notification
+
+# user instantiation
+append `. /sgoinfre/babelfi.sh` to your rc file
+
+# network installation
+```sh
+cd /sgoinfre
+git clone github.com/BeerB34r/babelfi.sh babelfish
+mkdir --mode=777 babelfish/present
+ln -s `realpath babelfish/babelfi.sh` babelfi.sh
+```
--- babelfi.sh
+++ babelfi.sh
@@ -12,9 +12,25 @@ appendMailpath() {
 		MAILPATH=$1?$2:${MAILPATH}
 	fi
 }
-appendMailpath $MYMAILBOX '[sgoinfre] Youve got mail from `stat --format="%U" $_`'$'\n''"`cat $_`"`\rm -f $_`'
-appendMailpath $MAILBOX'present' '$(grep -E "`test -z "${FRIENDS}" && echo -n "" || echo "${FRIENDS}|"`harl" $_ >/dev/null 2>/dev/null &&
-									echo "[present] `cat $_` is here!" ||
+
+babel() {
+	if [ -z "$1" -o -z "$2" ]; then
+		echo "[USAGE] babel \"<recipient>\" \"<subject>\""
+		return 1
+	elif [ ! -d "$MAILBOX/$1" ]; then
+		echo "[ERROR] can't find mailbox \"$1\""
+		return 1
+	elif [ "$1" = "present" ]; then
+		echo "[ERROR] don't write directly into \"present\""
+		return 1
+	fi
+	cat > $MAILBOX/"$1"/"$2"
+}
+
+
+appendMailpath $MYMAILBOX '[babelfish] Youve got mail from `stat --format="%U" "$_"`'$'\n''"`cat "$_"`"`\rm -f "$_"`'
+appendMailpath $MAILBOX'present' '$(grep -E "`test -z "${FRIENDS}" && echo -n "" || echo "${FRIENDS}|"`harl" "$_" >/dev/null 2>/dev/null &&
+									echo "[present] `cat "$_"` is here!" ||
 									printf "\033[A")'
 announcePresence() {
 	if [ "$1" ]; then
